a1478a2af7e033eadc2f2b83faafd23ea0fc04a3
lautarovollmer <lautarovollmer0@gmail.com>
2023-02-14 00:28:34 -0300

    Ejercicio 1 y 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ However, if you have any questions, please send us an email
 to support@webcat.app with the subject "Jr Fullstack Test Questions"
 */
 
-import $t from './libs/test.js'
+import $t from "./libs/test.js";
 
 /*
 1. Data manipulation:
@@ -33,12 +33,34 @@ import $t from './libs/test.js'
   Hint: Use native array methods as well as
     Lodash(https://lodash.com/docs) modules.
 */
-import _ from 'lodash'
-const source = $t.source(1)
+import _ from "lodash";
+const source = $t.source(1);
 $t.answer(1, async () => {
   // Your code goes here
-  return 
-})
+  const targetData = {
+    balance: 0,
+    income: 0,
+    expenses: 0,
+    byCategories: {
+      Restaurants: 0,
+      Income: 0,
+      Groceries: 0,
+      Rent: 0,
+    },
+  };
+
+  source.forEach((element) => {
+    if (element.type === "expense") {
+      targetData.expenses += element.amount;
+      targetData.byCategories[element.category] -= element.amount;
+    } else if (element.type === "income") {
+      targetData.income += element.amount;
+      targetData.byCategories[element.category] += element.amount;
+    }
+  });
+  targetData.balance = targetData.income - targetData.expenses;
+  return targetData;
+});
 
 /*
 2. Asynchronous programming: 
@@ -47,11 +69,17 @@ $t.answer(1, async () => {
   3. Finally, return the list of resulting texts as an array.
     
 */
-const $source = $t.source(2)
+const $source = $t.source(2);
 $t.answer(2, async () => {
-    // Your code goes here:
-    // 1. Get ids: $source.getIds()
-    // 2. Get text for every id: $source.getText(id)
-    // 3. Return array of texts
-    return 
-})
+  const ids = await $source.getIds();
+  const texts = await Promise.all(
+    ids.map(async (id) => {
+      return await $source.getText(id);
+    })
+  );
+  return texts;
+
+  // 1. Get ids: $source.getIds()
+  // 2. Get text for every id: $source.getText(id)
+  // 3. Return array of texts
+});
